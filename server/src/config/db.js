00dab9ba11eafc4f724af80d0e4f35eb2689d33d
mohamadalijomaa25 <82230445@students.liu.edu.lb>
2026-01-07 07:07:31 +0200

    Fix MySQL SSL + port for Aiven

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,13 +1,21 @@
 const mysql = require("mysql2/promise");
+const fs = require("fs");
+
+const ssl =
+  process.env.DB_SSL_CA_PATH
+    ? { ca: fs.readFileSync(process.env.DB_SSL_CA_PATH, "utf8") }
+    : undefined;
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT || 3306),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
 });
 
 module.exports = pool;
